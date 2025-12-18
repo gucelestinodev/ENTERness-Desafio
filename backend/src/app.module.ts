556c9b4da5b0b2bdf1d66aba8b7c47c3dc528983
gucelestinodev <gulima.cursos@gmail.com>
2025-12-18 20:00:50 -0300

    fix: :hammer: Ajuste para o deploy no Render.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ChatModule } from "./chat/chat.module";
+import { HealthController } from "./health.controller";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ChatModule,
   ],
+  controllers: [HealthController],
 })
 export class AppModule {}
